7948a07baaeab99b4e3cd245c32d234fcef7f5f3
Jotefde <jotefde.studio@gmail.com>
2018-01-24 22:49:33 +0100

    24.01.2018, 22:49

---
+++ Calc.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalc
+{
+    class Calc
+    {
+
+        public double Result { set; get; } = 0;
+        public Stack<KeyValuePair<Actions?, double>> History = new Stack<KeyValuePair<Actions?, double>>();
+        public enum Actions
+        {
+            Exit = '0',
+            Addition,
+            Subtraction,
+            Multiplication,
+            Division,
+            Modulo,
+            Exponentiation,
+            Root,
+            Undo,
+            Clear,
+            History = 'h'
+        };
+        public static Dictionary<Actions?, string> ActionDialogs = new Dictionary<Actions?, string>();
+        public Actions? Action { set; get; } = null;
+
+        public Calc()
+        {
+            fillActionDialogs();
+            Result = GetNumberDialog("Type the first number: ", "Only number type is expecting...");
+        }
+
+        private void fillActionDialogs()    
+        {
+            ActionDialogs.Add(Actions.Addition, "Type the number to add: ");
+            ActionDialogs.Add(Actions.Subtraction, "Type the number to subtract: ");
+            ActionDialogs.Add(Actions.Multiplication, "Type the second factor: ");
+            ActionDialogs.Add(Actions.Division, "Type the divider: ");
+            ActionDialogs.Add(Actions.Modulo, "Type the divider: ");
+            ActionDialogs.Add(Actions.Exponentiation, "Type the exponent: ");
+            ActionDialogs.Add(Actions.Root, "Type the degree of root: ");
+        }
+
+        public double GetNumberDialog(string mess, string warnMess )
+        {
+            bool doneAttempt = false;
+            string input;
+            double buff;
+
+            do
+            {
+                if (doneAttempt)
+                {
+                    Console.Clear();
+                    Console.WriteLine(warnMess);
+                }
+                Console.Write(mess);
+                input = Console.ReadLine();
+                doneAttempt = true;
+            } while (!Double.TryParse(input, out buff));
+            return buff;
+        }
+
+        public Actions MenuDialog(string dialogText)
+        {
+            Actions[] items = (Actions[])Enum.GetValues(typeof(Actions));
+            string itemName;
+            char inputKey;
+
+            do
+            {
+                this.ClearWindow();
+                Console.WriteLine(dialogText);
+                foreach (Actions item in items)
+                {
+                    itemName = item.ToString();
+                    Console.WriteLine(" {0}{1}",
+                        (char)item,
+                        itemName.PadLeft(20, '.'));
+                }
+                inputKey = Console.ReadKey().KeyChar;
+                //Int32.TryParse(inputKey.ToString(), out inputNumber);
+                
+            } while ( !Enum.IsDefined(typeof(Actions), (int)inputKey) && !Enum.IsDefined(typeof(Actions), inputKey+32));
+
+            return (Actions)inputKey;
+        }
+
+        public bool  Addition(double num)
+        {
+            Result += num;
+            return true;
+        }
+
+        public bool Subtraction(double num)
+        {
+            Result -= num;
+            return true;
+        }
+
+        public bool Multiplication(double num)
+        {
+            Result *= num;
+            return true;
+        }
+
+        public bool Division(double num)
+        {
+            if( num == 0 )
+            {
+                Console.WriteLine("You can't divide by zero!");
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+                return false;
+            }
+            Result /= num;
+            return true;
+        }
+
+        public bool Modulo(double num)
+        {
+            if (num == 0)
+            {
+                Console.WriteLine("You can't divide by zero!");
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+                return false;
+            }
+            Result %= num;
+            return true;
+        }
+
+        public bool Exponentiation(double num)
+        {
+            Result = Math.Pow(Result, num);
+            return true;
+        }
+
+        public bool Root(double num)
+        {
+            if (num == 0)
+            {
+                Console.WriteLine("There's no zero degree element!");
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+                return false;
+            }
+            Result = Math.Pow(Result, 1/num);
+            return true;
+        }
+
+        public void Clear()
+        {
+            Result = 0;
+            this.History.Clear();
+        }
+
+        public void ClearWindow()
+        {
+            Console.Clear();
+            Console.WriteLine("Result: {0}", this.Result);
+            Console.WriteLine("".PadRight(' ', '-'));
+        }
+
+        public void StoreResult(double val)
+        {
+            KeyValuePair<Actions?, double> kVp = new KeyValuePair<Actions?, double>(this.Action, val);
+            this.History.Push(kVp);
+        }
+
+        public void Undo()
+        {
+            KeyValuePair<Actions?, double> lastAction = this.History.Pop();
+            this.Result = lastAction.Value;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalc
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Simple Calculator";
+            Console.WriteLine($"Simple calculator in C#");
+            
+            Calc app = new Calc();
+            double buffInput,
+                   buffResult;
+            Type appType = app.GetType();
+            MethodInfo buffMethod;
+            string defaultWarnMess = "It's not number!";
+            bool methodDone = false;
+
+            do
+             {
+
+                if (app.Action == Calc.Actions.Exit)
+                    continue;
+
+                app.ClearWindow();
+
+                if (app.Action == Calc.Actions.Clear)
+                {
+                    app.Clear();
+                    app.Action = null;
+                }
+                else if (app.Action == Calc.Actions.History)
+                {
+                    foreach (KeyValuePair<Calc.Actions?, double> item in app.History)
+                    {
+                        Console.WriteLine("> {0} with before Result: {1}",
+                            item.Key,
+                            item.Value);
+                    }
+                    Console.ReadKey();
+                    app.Action = null;
+                }
+                else if (app.Action == Calc.Actions.Undo)
+                {
+                    Console.Write("Are you sure to Undo action? (y/n): ");
+                    char anw = Console.ReadKey().KeyChar;
+                    if(anw == 'y' || anw == 'Y')
+                        app.Undo();
+
+                    app.Action = null;
+                }
+                else if (app.Action != null)
+                {
+                    buffResult = app.Result;
+                    buffInput = app.GetNumberDialog(
+                        Calc.ActionDialogs[app.Action], defaultWarnMess);
+
+                    buffMethod = appType.GetMethod(app.Action.ToString());
+                    methodDone = Convert.ToBoolean( 
+                        buffMethod.Invoke(app, new Object[] { buffInput }) );
+
+                    if (methodDone)
+                    {
+                        app.StoreResult(buffResult);
+                        buffResult = 0;
+                    }
+                    app.Action = null;
+                }
+                else
+                {
+                    app.Action = app.MenuDialog("What do you want to perform?");
+                }
+
+            } while (app.Action != Calc.Actions.Exit);
+            
+        }
+    }
+}
